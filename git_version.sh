b663290fe27cff1593e82e5ca8ca29d10f91435c
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2025-03-20 21:13:24 +0900

    Refactor versioning logic in git_version.sh to encapsulate functionality in a get_version function and improve version output format

--- git_version.sh
+++ git_version.sh
@@ -13,34 +13,36 @@ function finally {
     exit 0
 }
 
-# Get the current branch
-branch=$(git rev-parse --abbrev-ref HEAD)
+function get_version {
+    branch=$(git rev-parse --abbrev-ref HEAD)
+    last_tag=$(git describe --tags --abbrev=0)
+    commit_count=$(git rev-list ${last_tag}..HEAD --count)
+    current_hash=$(git rev-parse --short HEAD)
+    version_nums=$(echo ${last_tag} | tr -d 'v')
+    major=$(echo ${version_nums} | cut -d. -f1)
+    minor=$(echo ${version_nums} | cut -d. -f2)
+    patch=$(echo ${version_nums} | cut -d. -f3)
+    changes=$(git status -s)
+    if [ -z "${changes}" ]; then
+        is_change=false
+    else
+        is_change=true
+    fi
+    if [ ${commit_count} -eq 0 ]; then
+        if [ ${is_change} = true ]; then
+            patch=$((patch + 1))
+            version="${major}.${minor}.${patch}.dev${commit_count}+d${current_hash}"
+        else
+            version="${major}.${minor}.${patch}"
+        fi
+    else
+        if [ ${is_change} = true ]; then
+            patch=$((patch + 1))
+        fi
+        version="${major}.${minor}.${patch}.dev${commit_count}+d${current_hash}"
+    fi
+    echo ${version}
+}
 
-# Get the last tag
-last_tag=$(git describe --tags --abbrev=0)
-
-# Get the commit count since the last tag
-commit_count=$(git rev-list ${last_tag}..HEAD --count)
-
-# Get the numbers of the version from tag
-version_nums=$(echo ${last_tag} | tr -d 'v')
-# Get the major, minor, and patch version from the last tag
-major=$(echo ${version_nums} | cut -d. -f1)
-minor=$(echo ${version_nums} | cut -d. -f2)
-patch=$(echo ${version_nums} | cut -d. -f3)
-# Get changes
-changes=$(git status -s)
-
-if [ -z "${changes}"]; then
-    is_change=false
-else
-    is_change=true
-fi
-
-echo "branch=${branch}"
-echo "last_tag=${last_tag}"
-echo "commit_count=${commit_count}"
-echo "major=${major}"
-echo "minor=${minor}"
-echo "patch=${patch}"
-echo "is_change=${is_change}"
+version=$(get_version)
+echo ${version}
